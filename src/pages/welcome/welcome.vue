0fe226591836a92582e4cc040fb46dfd19b31c36
Gridfas <809418027@qq.com>
2022-10-13 20:45:15 +0800

    优化代码逻辑

--- src/pages/welcome/welcome.vue
+++ src/pages/welcome/welcome.vue
@@ -26,7 +26,7 @@
                     <p @click="startGameAction" class="btn-title flex-c">
                         <span>开始挑战</span>
                     </p>
-                    <p class="version">版本号：1.0.101302</p>
+                    <p class="version">版本号：1.0.101303</p>
                 </div>
             </div>
         </div>
