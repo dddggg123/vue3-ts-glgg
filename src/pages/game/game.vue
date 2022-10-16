f69756d3cc9d7751ffad50d23836d3e7697b258d
Gridfas <809418027@qq.com>
2022-10-16 23:16:21 +0800

    优化代码

--- src/pages/game/game.vue
+++ src/pages/game/game.vue
@@ -155,7 +155,7 @@ const state = reactive({
             trap: false
         }
     ],
-    currentLevel: 2,
+    currentLevel: 0,
     currentDate: '- 10月10日 -',
     storePosition: {
         left: 0,
